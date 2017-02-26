c424303645ff26811392c0ab77be3375220cbebf
Koya Imakawa <koya.imakawa@g.softbank.co.jp>
2017-02-26 01:38:11 +0000

    Added want/have methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
 
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -31,20 +36,27 @@ class User < ActiveRecord::Base
 
   ## TODO 実装
   def have(item)
+    have_items.create(item_id: item)
   end
 
   def unhave(item)
+    have_items.find_by(item_id: item)
   end
 
   def have?(item)
+    have_items.include?(item)
   end
 
   def want(item)
+    want_items.create(item_id: item)
+
   end
 
   def unwant(item)
+    want_items.find_by(item_id: item)
   end
 
   def want?(item)
+    wants_items.include?(item)
   end
 end
