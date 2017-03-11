f5ea07eb621498aee207725c89f43231882c3ff4
Koya Imakawa <koya.imakawa@g.softbank.co.jp>
2017-03-11 02:36:59 +0000

    Corrected item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,6 @@ class Item < ActiveRecord::Base
   has_many :wants, class_name: "Want", foreign_key: "item_id", dependent: :destroy
   has_many :want_users , through: :wants, source: :user
   
-  has_many :have, class_name: "Have", foreign_key: "item_id", dependent: :destroy
+  has_many :haves, class_name: "Have", foreign_key: "item_id", dependent: :destroy
   has_many :have_users , through: :haves, source: :user
 end
